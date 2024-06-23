3eac44531bc91aad9314fcb152124d250123a881
Taku <86874120+Takubee@users.noreply.github.com>
2024-06-23 11:08:29 +0200

    Update transform_s4_teams_rankings.py

--- transform_s4_teams_rankings.py
+++ transform_s4_teams_rankings.py
@@ -1,29 +1,14 @@
-import boto3
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode
 
-# Initialize a session using Boto3
-session = boto3.Session()
-credentials = session.get_credentials()
-current_credentials = credentials.get_frozen_credentials()
-
 # Initialize Spark session builder
 spark_builder = SparkSession.builder.appName("MySparkSession")
-
-# Add S3 configuration for Spark session
-spark_builder.config("spark.hadoop.fs.s3a.access.key", current_credentials.access_key)
-spark_builder.config("spark.hadoop.fs.s3a.secret.key", current_credentials.secret_key)
-spark_builder.config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com")
-
-# Conditionally set the session token if it is available
-if current_credentials.token:
-    spark_builder.config("spark.hadoop.fs.s3a.session.token", current_credentials.token)
 
 # Create the Spark session
 spark = spark_builder.getOrCreate()
 
 # Path to your JSON file in S3
-s3_input_path = "s3a://formula-1-raw/events/season2024/teams_rankings.json"
+s3_input_path = "s3a://file-path"
 
 # Load JSON data from S3
 df = spark.read.format("json").option("multiLine", True).load(s3_input_path)
@@ -49,7 +34,7 @@ getColumns = exploded_df.select(
 getColumns.show()
 
 # Path to save the flattened data in S3
-s3_output_path = "s3a://formula-1-cleaned/events-cleaned/season2024-cleaned/teams-rankings-cleaned/"
+s3_output_path = "s3a://fs3-bucket-location/"
 
 # Write the flattened DataFrame to S3
 getColumns.write.format("parquet").mode("overwrite").save(s3_output_path)
